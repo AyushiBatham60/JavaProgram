51ab032a8e8b23218b696945b9d62ce1202b6751
AyushiBatham <ayushi.batham_cs19@gla.ac.in>
2023-01-04 19:33:52 +0530

    adding src folder

---
+++ src/Array/printArray.java
@@ -0,0 +1,16 @@
+package src.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class printArray {
+    public static void main(String[] args) {
+        int[] array=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+}
