05295e65e36556c0c547c1d815dd61e5a34beeaf
AyushiBatham <ayushi.batham_cs19@gla.ac.in>
2023-01-05 21:04:17 +0530

    adding medianOfTwoArray.java

--- src/Array/medianOfTwoArray.java
+++ src/Array/medianOfTwoArray.java
@@ -6,7 +6,7 @@ public class medianOfTwoArray {
     public static void main(String[] args) {
         int[] nums1 = {1,3};
         int[] nums2 = {2};
-
+        System.out.println(findMedianSortedArrays(nums1,nums2));
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
