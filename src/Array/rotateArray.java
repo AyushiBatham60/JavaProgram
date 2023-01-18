588d29789ff8871b5127d25d33e1c7f016b9fd3a
Ayushi Batham <ayushibatham60@gmail.com>
2023-01-19 01:32:16 +0530

    add rotateArray.java

---
+++ src/Array/rotateArray.java
@@ -0,0 +1,44 @@
+package src.Array;
+
+import java.util.Arrays;
+
+public class rotateArray {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50,60};
+        rotate_method_1(arr,4);
+        System.out.println(Arrays.toString(arr));
+
+        int[] arr2={10,20,30,40,50,60};
+        rotate_method_2(arr2,4);
+        System.out.println(Arrays.toString(arr2));
+    }
+    //o(n*n) complexity
+    public static void rotate_method_1(int[] arr,int ktime){
+        ktime=ktime%arr.length;
+        for (int j = 1; j <=ktime; j++) {
+            int temp=arr[arr.length-1];
+            for (int i = arr.length-1; i >=1; i--) {
+                arr[i]=arr[i-1];
+            }
+            arr[0]=temp;
+        }
+
+    }
+    public static void rotate_method_2(int[] arr,int ktime){
+        ktime=ktime%arr.length;
+        reverse(arr,0, arr.length-ktime-1);
+        reverse(arr,arr.length-ktime,arr.length-1);
+
+        reverse(arr,0,arr.length-1);
+    }
+    public static void reverse(int[] arr,int start,int end){
+        while (start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+}
