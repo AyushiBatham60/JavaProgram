764e3e7bfd9c5686c7afec687f430d8c07eabb6c
Ayushi Batham <ayushibatham60@gmail.com>
2023-02-01 22:18:22 +0530

    updating calculateTheSum.java

--- src/Array/calculateTheSum.java
+++ src/Array/calculateTheSum.java
@@ -1,10 +1,39 @@
 package src.Array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class calculateTheSum {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int testcases =sc.nextInt();
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]= sc.nextInt();
+        }
 
+        while (testcases >0){
+            calculate(arr,sc.nextInt());
+            System.out.println(Arrays.toString(arr));
+            testcases--;
+        }
     }
     public static void calculate(int[] array,int X){
-        
+        int[] copy=new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i]=array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            int indx=i-X;
+            if(indx<0){
+                indx+=array.length;
+            }
+            array[i]=copy[i]+copy[indx];
+        }
+        for (int i = 0; i < array.length; i++) {
+            copy[i]=array[i];
+        }
+
     }
 }
