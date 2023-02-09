1a0eaac2a397e476cb72b69db5f6b5e4f9bac490
Ayushi Batham <ayushibatham60@gmail.com>
2023-02-09 21:47:40 +0530

    updating intersectionOfArray.java

--- src/Array/intersectionOfArray.java
+++ src/Array/intersectionOfArray.java
@@ -1,6 +1,7 @@
 package src.Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,7 +18,7 @@ public class intersectionOfArray {
         for (int i = 0; i < m; i++) {
             array2[i]=sc.nextInt();
         }
-        intersection(array1,array2);
+        intersection2(array1,array2);
     }
     public static void intersection(int[] ar1,int[] ar2){
         List<Integer> ans=new ArrayList<>();
@@ -49,5 +50,31 @@ public class intersectionOfArray {
         for (int i = 0; i <ans.size() ; i++) {
             System.out.print(ans.get(i)+" ");
         }
+    }
+    public static int[] intersection2(int[] arr1,int[] arr2){
+        int p1=0;
+        int p2=0;
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        List<Integer> al=new ArrayList<>();
+
+        while (p1<arr1.length&& p2<arr2.length){
+            if(arr1[p1]==arr2[p2]){
+                al.add(arr1[p1]);
+                p1++;
+                p2++;
+            }
+            else if(arr1[p1]<arr2[p2]){
+                p1++;
+            }
+            else {
+                p2++;
+            }
+        }
+        int[] ans=new int[al.size()];
+        for (int i = 0; i < al.size(); i++) {
+            ans[i]=al.get(i);
+        }
+        return ans;
     }
 }
